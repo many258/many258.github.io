097794b9c58a2e94d25e561f9f2fd4b8190dbb6e
dino <many258@naver.com>
2021-01-23 20:22:09 +0900

    update

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Jekyll로 개인 블로그 시작해보기",
+        "excerpt":"임시 테스트 문서입니다.     수정 작업 필요  ","categories": ["life"],
+        "tags": ["Blog"],
+        "url": "/life/start-wtih-jekyll/",
+        "teaser": null
+      }]
