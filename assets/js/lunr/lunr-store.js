863e12ae9888b93b4bbbe4d0187a8ee634d20840
dino <many258@naver.com>
2021-01-25 21:01:30 +0900

    disqus error fixed & database contents upload

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,6 +2,12 @@ var store = [{
         "title": "Jekyll로 개인 블로그 시작해보기",
         "excerpt":"임시 테스트 문서입니다.     수정 작업 필요  ","categories": ["life"],
         "tags": ["Blog"],
-        "url": "/life/start-wtih-jekyll/",
+        "url": "https://many258.github.io//life/start-wtih-jekyll/",
+        "teaser": null
+      },{
+        "title": "[(구)정처기/컴퓨터일반] (1) 데이터베이스 자주 틀리는 내용 정리",
+        "excerpt":"주의) 해당 내용은 (구)정보처리기사, 컴퓨터일반 내용을 기반으로 문서화하였습니다. 수정이 필요하거나, 부족한 내용이 있을시 답글을 남겨주시면 해당 항목을 수정하겠습니다. 데이터 모델의 구성요소 구조(Structure) : 개념적 / 논리적 구성 형태 연산(Operation) : 개체 인스턴스의 삽입, 삭제, 검색, 갱신 처리 방법 제약사항(Constraint) : 개체 인스턴스의 허용 기준 E-R 관계 모델 원 : 속성...","categories": ["study"],
+        "tags": ["(구)정보처리기사","컴퓨터일반"],
+        "url": "https://many258.github.io//study/database/",
         "teaser": null
       }]
