dd1dc7bb3772dec1ec3d03e22796b6ccbfd36846
dino <many258@naver.com>
2021-02-04 00:52:10 +0900

    modify config.yml

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,54 +2,54 @@ var store = [{
         "title": "[(구)정처기/컴퓨터일반] (1) 데이터베이스 자주 틀리는 내용 정리",
         "excerpt":"주의) 해당 내용은 (구)정보처리기사, 컴퓨터일반 내용을 기반으로 문서화하였습니다. 수정이 필요하거나, 부족한 내용이 있을시 답글을 남겨주시면 해당 항목을 수정하겠습니다. 데이터 모델의 구성요소 구조(Structure) : 개념적 / 논리적 구성 형태 연산(Operation) : 개체 인스턴스의 삽입, 삭제, 검색, 갱신 처리 방법 제약사항(Constraint) : 개체 인스턴스의 허용 기준 E-R 관계 모델 원 : 속성...","categories": ["study"],
         "tags": ["(구)정보처리기사","컴퓨터일반"],
-        "url": "https://many258.github.io//study/database/",
+        "url": "https://many258.github.io/study/database/",
         "teaser": null
       },{
         "title": "[(구)정처기/컴퓨터일반] (2) 전자계산기 자주 틀리는 내용 정리",
         "excerpt":"주의) 해당 내용은 (구)정보처리기사, 컴퓨터일반 내용을 기반으로 문서화하였습니다. 수정이 필요하거나, 부족한 내용이 있을시 답글을 남겨주시면 해당 항목을 수정하겠습니다. 불 대수 기본 정리 A + A = A A · A = A A · 0 = 0 A + 0 = A A + 1 = 1 A · 1...","categories": ["study"],
         "tags": ["(구)정보처리기사","컴퓨터일반"],
-        "url": "https://many258.github.io//study/electronic-calculator/",
+        "url": "https://many258.github.io/study/electronic-calculator/",
         "teaser": null
       },{
         "title": "[(구)정처기/컴퓨터일반] (3) 운영체제 자주 틀리는 내용 정리",
         "excerpt":"주의) 해당 내용은 (구)정보처리기사, 컴퓨터일반 내용을 기반으로 문서화하였습니다. 수정이 필요하거나, 부족한 내용이 있을시 답글을 남겨주시면 해당 항목을 수정하겠습니다. 어셈블리어의 장점 어셈블리어로 작성한 원시 프로그램을 기계어로 번역 기계어보다 쉽고, 이해하기가 쉽다 프로그램의 주소가 기호 번지이다 프로그램에 데이터를 사용하기 쉽다 매크로 프로세서의 기능 매크로 정의 인식 : 매크로를 정의한 부분의 시작과 종료...","categories": ["study"],
         "tags": ["(구)정보처리기사","컴퓨터일반"],
-        "url": "https://many258.github.io//study/operating-system/",
+        "url": "https://many258.github.io/study/operating-system/",
         "teaser": null
       },{
         "title": "[(구)정처기/컴퓨터일반] (4) 소프트웨어 공학 자주 틀리는 내용 정리",
         "excerpt":"주의) 해당 내용은 (구)정보처리기사, 컴퓨터일반 내용을 기반으로 문서화하였습니다. 수정이 필요하거나, 부족한 내용이 있을시 답글을 남겨주시면 해당 항목을 수정하겠습니다. 소프트웨어 생명주기 나선형 모델 계획 수립(Planning) → 위험 분석(Risk analysis) → 개발(Engineering) → 고객 평가(Customer evaluation) 프로젝트 계획 단계의 특징 정의 단계의 시작 단계 개발 비용을 추정 유지보수 비용은 개발이 모두 끝난...","categories": ["study"],
         "tags": ["(구)정보처리기사","컴퓨터일반"],
-        "url": "https://many258.github.io//study/software-engineering/",
+        "url": "https://many258.github.io/study/software-engineering/",
         "teaser": null
       },{
         "title": "[(구)정처기/컴퓨터일반] (5) 데이터통신 자주 틀리는 내용 정리",
         "excerpt":"주의) 해당 내용은 (구)정보처리기사, 컴퓨터일반 내용을 기반으로 문서화하였습니다. 수정이 필요하거나, 부족한 내용이 있을시 답글을 남겨주시면 해당 항목을 수정하겠습니다. 네트워크 서비스 여러 개의 컴퓨터를 연결하여 각각에 연결된 컴퓨터의 하드웨어 자원과 소프트웨어 자원을 공유, 데이터나 파일을 공유 데이터 통신 시스템의 발전 SAGE : 미국의 군사용 방공 시스템, 최초의 데이터 통신 시스템 SABRE...","categories": ["study"],
         "tags": ["(구)정보처리기사","컴퓨터일반"],
-        "url": "https://many258.github.io//study/data-communication/",
+        "url": "https://many258.github.io/study/data-communication/",
         "teaser": null
       },{
         "title": "[토익/TOEIC] 문법 실전 압축",
         "excerpt":"주어 There is/are/remain/exist 뒤에는 명사 주어 가짜주어 it ⇒ (for+명사) to부정사 / that절 (빈칸) that S + (should)+동사원형 Insist / order / suggest / ask / request / demand It is (빈칸) that S + (should) 동사원형 Important / vital / imperative / necessary / essential / advised 명령문 ⇒...","categories": ["study"],
         "tags": ["TOEIC"],
-        "url": "https://many258.github.io//study/toeic-grammar/",
+        "url": "https://many258.github.io/study/toeic-grammar/",
         "teaser": null
       },{
         "title": "[(구)정처기/실기] 업무프로세스 키워드 정리",
         "excerpt":"주의) 해당 내용은 (구)정보처리기사, 컴퓨터일반 내용을 기반으로 문서화하였습니다. 수정이 필요하거나, 부족한 내용이 있을시 답글을 남겨주시면 해당 항목을 수정하겠습니다. ERP(Enterprise Resource Planning : 전사적 자원 관리) 기업 내의 모든 인적/물적 자원을 효율적으로 관리 경영자원을 하나의 체계로 통합 시스템을 재구축 → 생산성 극대화 BPR(Business Process Reengineering : 업무 재설계) 경영 전반을 분석하여...","categories": ["study"],
         "tags": ["(구)정보처리기사"],
-        "url": "https://many258.github.io//study/business-process/",
+        "url": "https://many258.github.io/study/business-process/",
         "teaser": null
       },{
         "title": "[(구)정처기/실기] 신기술동향 키워드 정리",
         "excerpt":"주의) 해당 내용은 (구)정보처리기사, 컴퓨터일반 내용을 기반으로 문서화하였습니다. 수정이 필요하거나, 부족한 내용이 있을시 답글을 남겨주시면 해당 항목을 수정하겠습니다. 통신 NFC(Near Field Communication : 근거리 무선 통신) 고주파를 이용한 근거리 무선 통신 기술, 가까운 거리에서 양방향 통신을 지원하는 RFID FemtoCell(펨토셀) 10-15을 뜻하는 펨토(Femto) + 이동통신에서 1개 기지국이 담당하는 서비스 구역(Cell) AP(Access...","categories": ["study"],
         "tags": ["(구)정보처리기사"],
-        "url": "https://many258.github.io//study/technology-trend/",
+        "url": "https://many258.github.io/study/technology-trend/",
         "teaser": null
       },{
         "title": "[Jekyll X BLOG] 나만의 블로그 설정 준비하기(feat. Jekyll)",
         "excerpt":"목차 Jekyll로 블로그 설정 준비하기 (2) 원하는 테마 찾아보기 (3) Git을 이용해서 실제 배포해보기 (4) Markdown 기본 문법 (5) 내 블로그 검색되게 해주세요(Google-Search/Naver-Advisor) 개요 나만의 블로그를 제작하는 방법은 여러가지가 있습니다. 예를 들자면, 네이버 블로그나 티스토리가 있죠 하지만 뭔가 심심한 느낌이 있고 내가 스스로 이것저것 만들고 바꿔보고 싶다면 개발자가 아니더라도 따라하면...","categories": ["blog"],
         "tags": ["Jekyll"],
-        "url": "https://many258.github.io//blog/start-wtih-jekyll/",
+        "url": "https://many258.github.io/blog/start-wtih-jekyll/",
         "teaser": null
       }]
